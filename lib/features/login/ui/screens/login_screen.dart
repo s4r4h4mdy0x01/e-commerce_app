9ad3321db962996cd4e7ae42ce0f3a59d590314f
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-06-04 19:20:09 +0300

    add login  screen ui

--- lib/features/login/ui/screens/login_screen.dart
+++ lib/features/login/ui/screens/login_screen.dart
@@ -1,10 +1,87 @@
+import 'package:e_commerce_app/core/helpers/spacing.dart';
+import 'package:e_commerce_app/core/string.dart';
+import 'package:e_commerce_app/core/theming/styles.dart';
+import 'package:e_commerce_app/core/widgets/app_text_button,dart';
+import 'package:e_commerce_app/core/widgets/app_text_form_field.dart';
+import 'package:e_commerce_app/features/login/ui/widgets/already_have_account.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isObscure = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 30.h, horizontal: 30.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                verticalSpace(100),
+                Text(
+                  StringManager.signIn,
+                  style: TextStyles.font24BlackBold,
+                ),
+                verticalSpace(20),
+                Column(
+                  children: [
+                    const AppTextFormField(
+                      hintText: 'Email',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    verticalSpace(8),
+                    AppTextFormField(
+                      hintText: 'Password',
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: isObscure,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isObscure = !isObscure;
+                          });
+                        },
+                        icon: Icon(
+                          isObscure ? Icons.visibility_off : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    verticalSpace(12),
+                    AppTextButton(
+                      buttonText: 'Login',
+                      textStyle: TextStyles.font16WhiteMedium,
+                      onPressed: () {},
+                    ),
+                    verticalSpace(10),
+                    //? todo
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: GestureDetector(
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font116orangRegular,
+                        ),
+                      ),
+                    ),
+                    verticalSpace(70),
+                    const AlreadyHaveAccount(),
+                    verticalSpace(70),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/login/ui/widgets/already_have_account.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+
+
+class AlreadyHaveAccount extends StatelessWidget {
+  const AlreadyHaveAccount({super.key});
+//  LoginScreen
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(children: [
+      TextSpan(
+          text: 'Already have an account?', style: TextStyles.font13Regular),
+      TextSpan(
+          text: 'Sign Up',
+          style: TextStyles.font13OrangSemiBold),
+    ]));
+  }
+}
